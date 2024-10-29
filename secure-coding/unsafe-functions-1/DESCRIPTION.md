754a4ecea2dc8c09943a8d0650567e577c9e9018
Steven Ngo <stengo46@gmail.com>
2024-10-29 02:10:22 -0700

    Clean up Unsafe Functions 1

--- secure-coding/unsafe-functions-1/DESCRIPTION.md
+++ secure-coding/unsafe-functions-1/DESCRIPTION.md
@@ -1,1 +1,5 @@
-Replace the unsafe function while maintaining functionality!
+In this challenge, the `list_files` function is vulnerable to OS command injection attacks because of the usage of `os.system(...)`. You are tasked with refactoring the code so that:
+1. `list_files` is no longer vulnerable to OS command injection attacks.
+2. `list_files` retains the original functionality to list all files in a given directory.
+
+The test cases in `unsafe_functions_1` should pass.
--- secure-coding/unsafe-functions-1/unsafe_code.py
+++ secure-coding/unsafe-functions-1/unsafe_code.py
@@ -1,5 +1,7 @@
 ### STARTING CODE ###
 """
+Note that the infrastructure will NOT let you modify any code in /challenge.
+
 Instructions:
 1) Create a file named "unsafe_code.py" in /home/hacker.
 2) Copy the below code and begin working there.
@@ -7,6 +9,6 @@ Instructions:
 #####################
 import os
 
-# Insecure Code: Uses os.system() to list files
+# Given a string directory value, list all the files using 'ls' with os.system(...).
 def list_files(directory):
     os.system(f'ls {directory}')
