598d646aee96384002a25f9ad19afa85a0c53aa8
Steven Ngo <stengo46@gmail.com>
2024-10-29 01:05:29 -0700

    Test update #7

--- secure-coding/unsafe-functions-1/codesafe/unsafe_code.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Insecure Code: Uses os.system() to list files
-def list_files(directory):
-    os.system(f'ls {directory}')
--- secure-coding/unsafe-functions-1/unsafe_code.py
+++ secure-coding/unsafe-functions-1/unsafe_code.py
@@ -1,3 +1,10 @@
+### STARTING CODE ###
+"""
+Instructions:
+1) Create a file named "unsafe_code.py" in /home/hacker.
+2) Copy the below code and begin working there.
+"""
+#####################
 import os
 
 # Insecure Code: Uses os.system() to list files
